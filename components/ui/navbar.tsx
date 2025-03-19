6a0692281e74c242f2a275a360f868408a217885
amkhrjee <aniruddha.mukherji@outlook.com>
2025-03-19 10:40:55 -0700

    add: avatars

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -1,6 +1,17 @@
 import { FaGithub } from "react-icons/fa6";
 import { Avatar, AvatarFallback, AvatarImage } from "./avatar";
 import { Button } from "./button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { HistoryIcon, LogOutIcon } from "lucide-react";
+import { createAvatar } from "@dicebear/core";
+import { lorelei } from "@dicebear/collection";
 
 export function Navbar() {
   return (
@@ -10,10 +21,28 @@ export function Navbar() {
         <Button variant={"outline"} size={"icon"}>
           <FaGithub size={24} />
         </Button>
-        <Avatar>
-          <AvatarImage src="https://github.com/shadcn.png" />
-          <AvatarFallback>CN</AvatarFallback>
-        </Avatar>
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Avatar>
+              <AvatarImage
+                src={createAvatar(lorelei, {
+                  seed: "test user",
+                }).toDataUri()}
+              />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="mr-4">
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <HistoryIcon /> History
+            </DropdownMenuItem>
+            <DropdownMenuItem variant="destructive">
+              <LogOutIcon /> Log out
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </div>
   );
