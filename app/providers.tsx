c93e0f64e7cbb41f575de48b20333ced985b5510
amkhrjee <aniruddha.mukherji@outlook.com>
2025-03-20 04:22:22 -0700

    add: footer + readme

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,13 +3,44 @@ import { ReactNode, useState } from "react";
 import { FilesContext } from "./context/FilesContext";
 import { SetFilesContext } from "./context/SetFilesContext";
 import { Files } from "@/config/definitions";
+import Link from "next/link";
+import { Badge } from "@/components/ui/badge";
 
 export function Providers({ children }: { children: ReactNode }) {
   const [files, setFiles] = useState<Files>(null);
   return (
     <FilesContext.Provider value={files}>
       <SetFilesContext.Provider value={setFiles}>
-        {children}
+        <div className="flex flex-col h-screen  justify-between">
+          {children}
+          <footer className="text-center p-4">
+            <p>Made for Tezpur University</p>
+            <p>
+              by{" "}
+              <Link
+                className="underline"
+                target="_blank"
+                href={"https://amkhrjee.xyz"}
+              >
+                Aniruddha
+              </Link>{" "}
+              &{" "}
+              <Link
+                className="underline"
+                target="_blank"
+                href={"https://linkedin.com/in/satyam-sajal-15a90325b"}
+              >
+                Satyam
+              </Link>
+            </p>
+            <p className="text-sm mt-2 text-gray-600">
+              v0.1.2{" "}
+              <Badge className="text-gray-600" variant={"outline"}>
+                beta
+              </Badge>
+            </p>
+          </footer>
+        </div>
       </SetFilesContext.Provider>
     </FilesContext.Provider>
   );
--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -1,18 +1,29 @@
 import { FaGithub } from "react-icons/fa6";
 import { Button, buttonVariants } from "./button";
 import Link from "next/link";
+import { ExternalLink } from "lucide-react";
 
 export function Navbar() {
   return (
     <div className="flex flex-row justify-between items-center p-4 mb-2">
       <p className="text-lg font-bold">📝 Autograder</p>
-      <Link
-        target="_blank"
-        href={"https://github.com/amkhrjee/autograder"}
-        className={buttonVariants({ variant: "outline" })}
-      >
-        <FaGithub />
-      </Link>
+      <div className="flex flex-row gap-2">
+        <Link
+          target="_blank"
+          href={"/system-design.png"}
+          className={buttonVariants({ variant: "outline" })}
+        >
+          Architecture <ExternalLink />
+        </Link>
+
+        <Link
+          target="_blank"
+          href={"https://github.com/amkhrjee/autograder"}
+          className={buttonVariants({ variant: "outline" })}
+        >
+          <FaGithub />
+        </Link>
+      </div>
     </div>
   );
 }
